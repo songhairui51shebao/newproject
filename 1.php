330f33f0454cfeb3c6f69b17f7a462a6b1460fb0
songhairui51shebao <songhairui@51shebao.com>
2015-06-29 11:29:34 +0800

    提交文件

---
+++ 1.php
@@ -0,0 +1,20 @@
+<?php
+
+$memcache = new Memcache();
+$memcache->connect("127.0.0.1",11211); # You might need to set "localhost" to "127.0.0.1"
+	
+echo "Server's version: " . $memcache->getVersion() . "\n";
+
+$tmp_object = new stdClass;
+$tmp_object->str_attr = "test";
+$tmp_object->int_attr = 123;
+
+$memcache->set("key",$tmp_object,false,10);
+echo "Store data in the cache (data will expire in 10 seconds)\n";
+
+echo "Data from the cache:\n";
+var_dump($memcache->get("key"));
+
+
+echo 1;
+?>
